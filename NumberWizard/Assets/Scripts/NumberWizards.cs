879066fccdb4f49b3d30285fecd9a0c5645c9ab5
evan_localadmin <evan_localadmin@mpd-mac-10.mill.co.uk>
2018-02-23 17:09:44 +0000

    Initial commit

---
+++ NumberWizard/Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizards : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Welcome to Numer Wizard");
+		print("Pick a number in your head but don't tell me.");
+
+		int max = 1000;
+		int min = 1;
+
+		print("the highest number you can pick is " + max);
+		print("the lowest number you can pick is " + min);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
